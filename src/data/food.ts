f052ccb1929c84514e2fca2cfd662a435a7a1af3
Scott Jackson <daneren2005@gmail.com>
2023-11-07 22:15:03 -0800

    Get infinite loop of food dropping down

--- src/data/food.ts
+++ src/data/food.ts
@@ -5,13 +5,26 @@ const foods: Array<Item> = [
 		name: 'Bannana',
 		key: 'food',
 		frame: 0,
-		weight: 50
+		weight: 472
 	},
 	{
 		name: 'Orange',
 		key: 'food',
 		frame: 1,
-		weight: 20
+		weight: 120
+	},
+	{
+		name: 'Apple',
+		key: 'food',
+		frame: 2,
+		weight: 70
+	},
+	// TODO: This blows everything out of the water.  Should we make these rarer or just pretend they are a lot lighter?
+	{
+		name: 'Watermelon',
+		key: 'food',
+		frame: 2,
+		weight: 10_000
 	}
 ];
 
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -3,9 +3,11 @@ import items from '../data/items';
 import randomElement from '../utils/random-element';
 
 export default class GameScene extends Phaser.Scene {
-	leftPosition: number = 0;
-	rightPosition: number = 0;
-	startHeight: number = 0;
+	position: number = 0;
+	visiblePositions: Array<number> = [];
+	startHeight: number = 30;
+
+	fallingSprites: Array<Phaser.Types.Physics.Arcade.ImageWithDynamicBody> = [];
 
 	constructor() {
 		super('game');
@@ -19,22 +21,55 @@ export default class GameScene extends Phaser.Scene {
 	}
 
 	create() {
-		this.initLeftRightPositions();
+		this.initKeys();
+		this.initPositions(2);
 		this.spawnItem();
+
+		this.physics.world.on('worldbounds', (body: any) => {
+			let sprite = body.gameObject as Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
+			let index = this.fallingSprites.indexOf(sprite);
+			if(index !== -1) {
+				this.fallingSprites.splice(index);
+			}
+			body.onWorldBounds = false;
+			this.spawnItem();
+		});
 	}
 
-	initLeftRightPositions() {
+	initKeys() {
+		this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.A).on('down', () => {
+			this.incPosition(-1);
+		});
+		this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.D).on('down', () => {
+			this.incPosition(1);
+		});
+	}
+	incPosition(inc: -1 | 1) {
+		this.position = Math.max(0, Math.min(this.visiblePositions.length - 1, this.position + inc));
+
+		this.fallingSprites.forEach(sprite => {
+			sprite.x = this.visiblePositions[this.position];
+		});
+	}
+
+	initPositions(count: number) {
 		let { width } = this.sys.game.canvas;
 
-		this.leftPosition = width / 3;
-		this.rightPosition = this.leftPosition * 2;
+		let widthPortion = width / (count + 1);
+		this.visiblePositions = [];
+		for(let i = 0; i < count; i++) {
+			this.visiblePositions.push(widthPortion * (i + 1));
+		}
 	}
 
 	spawnItem() {
 		let item = randomElement(items);
-		const sprite = this.physics.add.image(this.leftPosition, this.startHeight, item.key, item.frame);
+		const sprite = this.physics.add.image(this.visiblePositions[this.position], this.startHeight, item.key, item.frame);
 
 		sprite.setVelocity(0, 200);
 		sprite.setCollideWorldBounds(true);
+		sprite.body.onWorldBounds = true;
+		this.fallingSprites.push(sprite);
 	}
 }
